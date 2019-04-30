c2cfeca91ef03a8382af0b094e613aeb4ec8136e
Francis Tseng <f+accounts@frnsys.com>
2019-04-29 20:07:39 -0400

    cleanup, after realizing that stripe handles a lot of this :)

--- shop/forms.py
+++ shop/forms.py
@@ -1,24 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms.validators import InputRequired, Email
-from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms.fields import TextField, TextAreaField, IntegerField, BooleanField
-
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+from wtforms.fields import TextField
+from wtforms.validators import Email
 
 class CancelSubscriptionForm(FlaskForm):
     email = TextField('Email', [Email()])
-
-class ProductForm(FlaskForm):
-    name = TextField('Name', [InputRequired()])
-    desc = TextAreaField('Description', [InputRequired()])
-    price = IntegerField('Price (cents)', [InputRequired()])
-    published = BooleanField('Published')
-
-class SKUForm(FlaskForm):
-    name = TextField('Name', [InputRequired()])
-    stock = IntegerField('Stock', [InputRequired()])
-
-class ImageForm(FlaskForm):
-    file = FileField('Image', [FileRequired(),
-                               FileAllowed(ALLOWED_EXTENSIONS, 'Images only')])
-
