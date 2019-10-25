7491dd9e35c57edc54be52841fd113c9f175400d
Francis Tseng <f+accounts@frnsys.com>
2019-10-25 16:27:43 -0400

    method to get product by id

--- konbini/core.py
+++ konbini/core.py
@@ -5,3 +5,6 @@ def get_products():
 
 def get_plans():
     return stripe.Product.list(limit=100, active=True, type='service')['data']
+
+def get_product(id):
+    return stripe.Product.retrieve('prod_{}'.format(id))
