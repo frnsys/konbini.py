59c4efefbb84cc305db0cfdbb69efbc5680ee069
Francis Tseng <f+accounts@frnsys.com>
2019-10-28 11:48:45 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='konbini',
-    version='0.0.1',
+    version='0.0.2',
     description='simple web store for flask',
     url='https://github.com/frnsys/konbini',
     author='Francis Tseng',
