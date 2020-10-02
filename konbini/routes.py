c8d7272ee81b62333f3b03296cd99ef82bf4012e
Francis Tseng <f+accounts@frnsys.com>
2020-10-02 12:21:20 -0400

    reorganized

--- konbini/routes.py
+++ konbini/routes.py
@@ -20,81 +20,6 @@ USPS_ADDRESS_KEYS = {
 bp = Blueprint('shop', __name__, template_folder='templates')
 
 
-def get_shipping_rate(products, addr):
-    """Estimate a shipping rate for a product.
-    This does not actually purchase shipping, this is just to figure out
-    how much to charge for it."""
-    package_dimensions = [{
-        k: float(p.metadata.get(k))
-        for k in ['height', 'weight', 'length', 'width']
-    } for p, _ in products]
-
-    # ENH This should use some 3d bin packing algorithm
-    # but for now just take the largest product volume
-    # ENH this should take quantity into account
-    dimensions = max(package_dimensions,
-            key=lambda p: p['height'] * p['weight'] * p['length'])
-
-    # We can at least sum the weights
-    total_weight = sum(p['weight'] for p in package_dimensions)
-    dimensions['weight'] = total_weight
-
-    kwargs = {
-        'from_address': current_app.config['KONBINI_SHIPPING_FROM'],
-        'to_address': {
-            'name': addr['name'],
-            'street1': addr['address']['line1'],
-            'street2': addr['address'].get('line2'),
-            'city': addr['address']['city'],
-            'state': addr['address']['state'],
-            'zip': addr['address']['postal_code'],
-            'country': addr['address']['country']
-        },
-        'parcel': dimensions,
-    }
-
-    # https://www.easypost.com/customs-guide
-    if addr['address']['country'] != 'US' and 'KONBINI_CUSTOMS' in current_app.config:
-        customs_items = []
-        for (product, quantity), dims in zip(products, package_dimensions):
-            # ENH this can be improved
-            # Grab first SKU to get price
-            if product['type'] == 'good':
-                skus = stripe.SKU.list(limit=100, product=product.id, active=True)['data']
-                if skus:
-                    price = skus[0].price/100 # cents to USD
-                else:
-                    prices = stripe.Price.list(limit=100, product=product.id, active=True)['data']
-                    price = prices[0].unit_amount/100 # cents to USD
-            else:
-                prices = stripe.Price.list(limit=100, product=product.id, active=True)['data']
-                price = prices[0].unit_amount/100 # cents to USD
-
-            # Create customs item. We are making a few assumptions here
-            customs_item = easypost.CustomsItem.create(
-                quantity=quantity,
-                description=product.description,
-                value=price,
-                weight=dims['weight'],
-                code=product.id,
-                origin_country='US', # NOTE assumed to be US
-                # hs_tariff_number # isn't required by easypost
-            )
-            customs_items.append(customs_item)
-        customs_info = easypost.CustomsInfo.create(
-            customs_items=customs_items,
-            **current_app.config['KONBINI_CUSTOMS']
-        )
-        kwargs['customs_info'] = customs_info
-    shipment = easypost.Shipment.create(**kwargs)
-
-    # Get cheapest rate
-    rate = min(float(r.rate) for r in shipment.rates)
-
-    # Convert to cents
-    return math.ceil(rate*100), shipment.id
-
-
 def is_in_stock(sku):
     inv = sku['inventory']
     if inv['type'] == 'bucket' and inv['value'] == 'out_of_stock':
@@ -268,7 +193,7 @@ def pay():
 
     products = [(stripe.Product.retrieve(session['meta'][sku_id]['product_id']), quantity)
             for sku_id, quantity in session['cart'].items()]
-    rate, shipment_id = get_shipping_rate(products, session['shipping'])
+    rate, shipment_id = core.get_shipping_rate(products, session['shipping'], **current_app.config)
 
     items = [{
         'currency': 'usd',
@@ -375,7 +300,7 @@ def subscribe_invoice_hook():
                 # Calculate shipping estimate
                 prod_id = prod['metadata']['shipped_product_id']
                 product = stripe.Product.retrieve(prod_id)
-                rate, _ = get_shipping_rate([(product, 1)], addr)
+                rate, _ = core.get_shipping_rate([(product, 1)], addr, **current_app.config)
 
                 # Add the item to this invoice
                 stripe.InvoiceItem.create(
@@ -528,7 +453,7 @@ def subscribe():
             plan_prod = stripe.Product.retrieve(prod_id)
             prod_id = plan_prod['metadata']['shipped_product_id']
             product = stripe.Product.retrieve(prod_id)
-            rate, shipment_id = get_shipping_rate([(product, 1)], addr)
+            rate, shipment_id = core.get_shipping_rate([(product, 1)], addr, **current_app.config)
             line_items.append({
                 'name': 'Shipping',
                 'description': 'Shipping',
