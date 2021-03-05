032f5f01716fafcf0ed1e20b10ce04b512401d9a
Francis Tseng <f+accounts@frnsys.com>
2021-03-05 09:58:48 -0500

    shipper config key fix

--- konbini/shipping/__init__.py
+++ konbini/shipping/__init__.py
@@ -2,9 +2,9 @@ import importlib
 from flask import current_app
 
 def get_shipping_rate(products, addr, **config):
-    shipper = importlib.import_module(current_app.config['SHIPPER'])
+    shipper = importlib.import_module(current_app.config['KONBINI_SHIPPER'])
     return shipper.get_shipping_rate(products, addr, **config)
 
 def buy_shipment(shipment_id, **kwargs):
-    shipper = importlib.import_module(current_app.config['SHIPPER'])
+    shipper = importlib.import_module(current_app.config['KONBINI_SHIPPER'])
     return shipper.buy_shipment(shipment_id, **kwargs)
