16d03f8cba26097158ac6ef8780ab3a33a88fbbc
Francis Tseng <f+accounts@frnsys.com>
2021-03-10 08:29:27 -0500

    shipper module import fix

--- konbini/shipping/__init__.py
+++ konbini/shipping/__init__.py
@@ -2,9 +2,9 @@ import importlib
 from flask import current_app
 
 def get_shipping_rate(products, addr, **config):
-    shipper = importlib.import_module(current_app.config['KONBINI_SHIPPER'])
+    shipper = importlib.import_module('.'+current_app.config['KONBINI_SHIPPER'], 'konbini.shipping')
     return shipper.get_shipping_rate(products, addr, **config)
 
 def buy_shipment(shipment_id, **kwargs):
-    shipper = importlib.import_module(current_app.config['KONBINI_SHIPPER'])
+    shipper = importlib.import_module('.'+current_app.config['KONBINI_SHIPPER'], 'konbini.shipping')
     return shipper.buy_shipment(shipment_id, **kwargs)
