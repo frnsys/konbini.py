9ad51cd7de94642aaee470c7c58d245bfd605abd
Francis Tseng <f+accounts@frnsys.com>
2021-03-15 08:44:10 -0400

    added back in exclude_tax key for items

--- konbini/routes.py
+++ konbini/routes.py
@@ -203,7 +203,7 @@ def pay():
         'name': session['meta'][sku_id]['name'],
         'amount': session['meta'][sku_id]['price'],
         'quantity': quantity,
-        # 'exclude_tax': session['meta'][sku_id]['exclude_tax'],
+        'exclude_tax': session['meta'][sku_id]['exclude_tax'],
     } for sku_id, quantity in session['cart'].items()]
 
     items.append({
@@ -211,7 +211,7 @@ def pay():
         'name': 'Shipping',
         'amount': rate,
         'quantity': 1,
-        # 'exclude_tax': True
+        'exclude_tax': True
     })
     total = sum(i['amount'] for i in items)
     tax_total = sum(i['amount'] for i in items if not i['exclude_tax'])
