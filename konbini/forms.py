f8bb2ceeed9f2bd14294c1ae84e3f24332736e66
Francis Tseng <f+accounts@frnsys.com>
2023-11-27 17:52:10 +0100

    updated wtforms

--- konbini/forms.py
+++ konbini/forms.py
@@ -1,24 +1,23 @@
 from flask_wtf import FlaskForm
 from country_list import countries_for_language
 from wtforms.validators import Email, Length, InputRequired
-from wtforms.fields import TextField, FormField, SelectField, HiddenField
-from wtforms.fields.html5 import EmailField
+from wtforms.fields import StringField, FormField, SelectField, HiddenField, EmailField
 
 countries = countries_for_language('en')
 
 class CancelSubscriptionForm(FlaskForm):
-    email = TextField('Email', [Email()])
+    email = StringField('Email', [Email()])
 
 class AddressForm(FlaskForm):
-    line1 = TextField('Address', [InputRequired()])
-    line2 = TextField('Address 2')
-    city = TextField('City', [InputRequired()])
-    state = TextField('State', [Length(max=2)]) # Not always used in int'l addresses. Shipbob requires 2 character state abbreviations.
-    postal_code = TextField('Zipcode', [InputRequired()])
+    line1 = StringField('Address', [InputRequired()])
+    line2 = StringField('Address 2')
+    city = StringField('City', [InputRequired()])
+    state = StringField('State', [Length(max=2)]) # Not always used in int'l addresses. Shipbob requires 2 character state abbreviations.
+    postal_code = StringField('Zipcode', [InputRequired()])
     country = SelectField('Country', [InputRequired()], choices=countries, default='US')
 
 class ShippingForm(FlaskForm):
-    name = TextField('Name', [InputRequired()])
+    name = StringField('Name', [InputRequired()])
     email = EmailField('Email', [InputRequired(), Email()])
     address = FormField(AddressForm)
 
